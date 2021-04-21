896427021db16aa021bfb3128a76c6782ab94ba9
Dilshad mohammed <69088425+dilshadmohammed@users.noreply.github.com>
2021-04-21 20:39:25 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -14,23 +14,12 @@ function buttonEqual(){
     
 }
 
-function downloadImage() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://via.placeholder.com/150', true);
-  xhr.responseType = 'blob';
-  xhr.onload = function() {
-    var urlCreator = window.URL || window.webkitURL;
-    var imageUrl = urlCreator.createObjectURL(this.response);
-    var tag = document.createElement('a');
-    tag.href = https://lcm.scienceinstitute.in/online-videos/videos/1617186816ALDEHYDES%20KETONES%20AND%20CARBOXYLIC%20ACIDS%20CSJ%20part%2010.mp4;
-    tag.target = '_blank';
-    tag.download = 'sample.png';
-    document.body.appendChild(tag);
-    tag.click();
-    document.body.removeChild(tag);
-  };
-  xhr.onerror = err => {
-    alert('Failed to download video');
-  };
-  xhr.send();
-}
+<script>
+jQuery(document).ready(function() {
+  var downloadButton = jQuery('.et-download-button');
+   
+  downloadButton.each(function(index) {
+    jQuery(this).attr('download', 'image-file');
+  });
+});
+</script>
